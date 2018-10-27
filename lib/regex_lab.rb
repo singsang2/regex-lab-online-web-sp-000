06cccf487db638c83dc2b9666c312681e90d5dd5
Sung Bae <singsang2@gmail.com>
2018-10-27 13:13:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/\b[AEIOUaeiou]\w+\b/) == nil ? false : true
+  word.match(/\b[AEIOUaeiou]\w*\b/) == nil ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
