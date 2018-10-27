0a6ad74acffb508895e1ee878e4158fcf45b7ee3
Sung Bae <singsang2@gmail.com>
2018-10-27 13:21:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[AEIOUaeiou]\w*\b/) == nil ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ng\b/)
 end
 
-def words_five_letters_long(text)
 
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\b[A-Z]\w*[!"#$%&'()*+,.:;<=>?@\^_`{|}~]\s\b/)==nil ? false : true
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\W?\d{3}\W?\d{3}\W?\W?\d{4}\W?/) == nil ? false : true
 end
+
+puts words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
